9c3ea6efab4e66f169477e3c36c4da1cb63f2a30
Santiago Aquino <santiagoaquino01@hotmail.com>
2022-07-11 18:29:35 -0300

    Subiendo 1era entrega complementaria

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// entrega complementaria
+
+/* let numero = prompt('Ingrese la cantidad de repeticones que quiere que diga "OSO"');
+
+for(let i = 0; i < numero; i++){
+    alert('OSO');
+}
+
+alert('Adios'); */
+
+let apellidos = prompt('Ingrese apellidos que se le ocurra');
+
+while(apellidos != 'aquino'){
+    alert(apellidos)
+    apellidos = prompt('Ingrese apellidos que se le ocurra');
+}
+
+alert('Adios');
